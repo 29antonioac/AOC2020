d9d00ad2183bab3cda5f76f25282076773cace58
Antonio Álvarez Caballero <analca3@gmail.com>
2020-12-23 16:29:12 +0000

    Refactored to use linked lists, now it's fast!

--- Day23/Day23.py
+++ Day23/Day23.py
@@ -1,96 +1,99 @@
 #!/usr/bin/env python
 
-import os
-import re
-import sys
-import math
-import requests
-import itertools
-import functools
-
 from typing import List, Tuple, Dict, Set
-from copy import deepcopy
-
-
-sys.path.append("..")
-from utils import download_input
 
 INPUT_URL = "https://adventofcode.com/2020/day/22/input"
 INPUT_STR = "389547612"
 # INPUT_STR = "389125467"
 
 ##################################################
-def parse_input(numbers: str) -> Dict:
-    return [int(i) for i in numbers]
+class Cup(object):
+    def __init__(self, label, next=None):
+        self.label = label
+        self.next = next
 
-@functools.cache
-def move(original_numbers: Tuple[int], current: int) -> List[int]:
-    numbers = list(original_numbers)
-    # print(f"cups: {numbers}")
-    # print(f"current: {current}")
-    current_idx = numbers.index(current)
-    picked = [numbers[(current_idx + 1) % len(numbers)],
-                numbers[(current_idx + 2) % len(numbers)],
-                numbers[(current_idx + 3) % len(numbers)]]
-    for p in picked:
-        numbers.pop(numbers.index(p))
-    # print(f"pick up: {picked}")
-    destination_idx = None
-    destination_number = current - 1
-    # print(f"May I have destination {destination_number}?")
-    while (destination_idx is None) or destination_number in picked:
-        # print(f"{destination_idx} -> {destination_number}")
-        try:
-            destination_idx = numbers.index(destination_number)
-        except:
-            if destination_number < 0:
-                destination_number = max(numbers)
-            else:
-                destination_number = destination_number - 1
-            # print(f"May I have destination {destination_number}?")
-    # print(f"destination: {destination_number}")
+    def change_next(self, next):
+        self.next = next
 
-    numbers.insert(destination_idx + 1, picked[0])
-    numbers.insert(destination_idx + 2, picked[1])
-    numbers.insert(destination_idx + 3, picked[2])
-    new_current = numbers[(numbers.index(current) + 1) % len(numbers)]
-    return numbers, new_current
+    def __str__(self):
+        return str(self.label) + "->" + (str(self.next.label) if self.next else "None")
 
-def play(numbers: List[int], times: int) -> List[int]:
-    new_current = numbers[0]
+    def __repr__(self):
+        return str(self.label) + "->" + (str(self.next.label) if self.next else "None")
+
+def parse_input(numbers: str) -> Dict[int, Cup]:
+    number_list = [int(i) for i in numbers]
+    cup_list = {i: Cup(i) for i in number_list}
+    number_list.append(number_list.pop(0))
+    for idx, idx_next in zip(cup_list.keys(), number_list):
+        cup_list[idx].change_next(cup_list[idx_next])
+    return cup_list
+
+def move(cup_list: Dict[int, Cup], cup: Cup) -> Cup:
+
+    picked_cups = [cup.next, cup.next.next, cup.next.next.next]
+    picked_numbers = [c.label for c in picked_cups]
+
+    destination_number = cup.label - 1
+    destination_cup = cup_list.get(destination_number, None)
+
+    while (destination_cup is None) or destination_number in picked_numbers:
+        if destination_number <= 0:
+            destination_number = max(cup_list.keys())
+        else:
+            destination_number = destination_number - 1
+        destination_cup = cup_list.get(destination_number, None)
+
+    cup.change_next(picked_cups[-1].next)
+    picked_cups[-1].change_next(destination_cup.next)
+    destination_cup.change_next(picked_cups[0])
+
+    new_current = cup.next
+    return new_current
+
+def play(cup_list: List[Cup], times: int) -> List[int]:
+    current_cup = cup_list[3]
     for _ in range(times):
-        numbers, new_current = move(tuple(numbers), new_current)
+        current_cup = move(cup_list, current_cup)
 
-    return numbers
+    return cup_list
 
 
 
 ####################################
 
-numbers = parse_input(INPUT_STR)
+cup_list_one = parse_input(INPUT_STR)
 
 # Part one
-final_numbers_one = play(numbers, 100)
+final_numbers_one = play(cup_list_one, 100)
 
-idx_n = (final_numbers_one.index(1) + 1) % len(final_numbers_one)
-n = final_numbers_one[idx_n]
+cup = final_numbers_one[1].next
 final_numbers_sorted = []
-while n != 1:
-    final_numbers_sorted.append(n)
-    idx_n = (idx_n + 1) % len(final_numbers_one)
-    n = final_numbers_one[idx_n]
+while cup.label != 1:
+    final_numbers_sorted.append(cup.label)
+    cup = cup.next
 
 final_result = "".join([str(i) for i in final_numbers_sorted])
 print(f"Result after 100 moves is {final_result}")
 
 # Part two
-n_moves = 10000
-# new_numbers = numbers + list(range(max(numbers), 1000000 + 1))
-new_numbers = numbers + list(range(max(numbers), 10000 + 1))
-final_numbers_two = play(new_numbers, n_moves)
+n_moves = 10000000
+size = 1000000
 
-idx_n1 = (final_numbers_two.index(1) + 1) % len(final_numbers_two)
-idx_n2 = (idx_n1 + 1) % len(final_numbers_two)
+cup_list_two = parse_input(INPUT_STR)
+existent = max(cup_list_two.keys())
+for i in range(existent + 1, size + 1):
+    cup_list_two[i] = Cup(i)
 
-final_result_two = final_numbers_two[idx_n1] * final_numbers_two[idx_n2]
+cup_list_two[int(INPUT_STR[-1])].change_next(cup_list_two[existent+1])
+
+for i in range(existent+1, size):
+    cup_list_two[i].change_next(cup_list_two[i+1])
+cup_list_two[size].change_next(cup_list_two[3])
+
+final_numbers_two = play(cup_list_two, n_moves)
+
+cup_one = cup_list_two[1]
+
+final_result_two = cup_one.next.label * cup_one.next.next.label
 print(f"Result after {n_moves} moves is {final_result_two}")
