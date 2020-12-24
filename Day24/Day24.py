2f63a5bc77c34469cab67b9ebc358c93560a8e81
Antonio Álvarez Caballero <analca3@gmail.com>
2020-12-24 11:08:45 +0000

    Added part2, with sets is as easy as Day17

--- Day24/Day24.py
+++ Day24/Day24.py
@@ -6,6 +6,7 @@ import sys
 import math
 import requests
 
+from collections import Counter
 from typing import List, Tuple, Dict, Set
 from copy import deepcopy
 
@@ -17,22 +18,13 @@ INPUT_URL = "https://adventofcode.com/2020/day/24/input"
 INPUT_FILENAME = "input.txt"
 # INPUT_FILENAME = "example_input.txt"
 
-nw = "nw"
-ne = "ne"
-w = "w"
-e = "e"
-sw = "sw"
-se = "se"
+DIRECTIONS = {  "nw": (0,-1),
+                "ne": (1,-1),
+                "w": (-1,0),
+                "e": (1,0),
+                "sw": (-1,1),
+                "se": (0,1)}
 
-DIRECTIONS = {  nw: (0,-1),
-                ne: (1,-1),
-                w: (-1,0),
-                e: (1,0),
-                sw: (-1,1),
-                se: (0,1)}
-
-COLOURS = {"black": 1,
-            "white": 0}
 
 Tile = Tuple[int,int]
 
@@ -45,7 +37,6 @@ def parse_input(filename: str) -> Dict:
 
 def follow_rules(rules: List[List[str]]) -> Set[Tile]:
     current_tile = (0,0)
-    # tiles = {current_tile: COLOURS["white"]}
     tiles = set()
 
     for tile in rules:
@@ -53,27 +44,38 @@ def follow_rules(rules: List[List[str]]) -> Set[Tile]:
         for direction in tile:
             increment = DIRECTIONS[direction]
             current_tile = tuple([t+i for t,i in zip(current_tile, increment)])
-            # if current_tile in tiles and tiles[current_tile] == COLOURS["black"]:
-            #     print(f"{current_tile} was black, now white")
-            # if current_tile not in tiles:
-            #     tiles[current_tile] = COLOURS["white"]
-        # print(f"Flipping {current_tile} to black")
+
         if current_tile not in tiles:
             tiles.add(current_tile)
         else:
             tiles.remove(current_tile)
-        # tiles[current_tile] = COLOURS["black"] if tiles[current_tile] == COLOURS["white"] else COLOURS["white"]
-        # print()
 
     return tiles
 
 def evolve(original_tiles: Dict[Tile, int], n_times: int) -> Dict[Tile, int]:
+    def get_neighbours(tile: Tile):
+        # This could have been done with zip, too, as above
+        neighbours = [(tile[0] + inc[0], tile[1] + inc[1]) for inc in DIRECTIONS.values()]
+        return neighbours
+
     tiles = deepcopy(original_tiles)
-    return
+    for _ in range(n_times):
+        neighbour_counter = Counter()
 
+        to_remove = set()
+        to_add = set()
 
+        # Same approach as Day17: count the neighbours of my neighbours
+        for t in tiles:
+            for neighbour in get_neighbours(t):
+                neighbour_counter[neighbour] += 1
 
+        to_remove = set([t for t in tiles if neighbour_counter[t] == 0 or neighbour_counter[t] > 2])
+        to_add = set([t for t,c in neighbour_counter.items() if t not in tiles and neighbour_counter[t] == 2])
 
+        tiles = tiles.union(to_add) - to_remove
+
+    return tiles
 
 ####################################
 if not os.path.exists(INPUT_FILENAME):
@@ -83,8 +85,15 @@ if not os.path.exists(INPUT_FILENAME):
         exit(-1)
 
 rules = parse_input(INPUT_FILENAME)
+
+# Part one
 tiles = follow_rules(rules)
 
-# black_tiles = sum([c for c in tiles.values() if c == COLOURS["black"]])
 black_tiles = len(tiles)
 print(f"The sum of black tiles is {black_tiles}")
+
+# Part two
+n_times = 100
+final_tiles = evolve(tiles, n_times=n_times)
+final_black_tiles = len(final_tiles)
+print(f"The sum of black tiles after {n_times} days is {final_black_tiles}")
